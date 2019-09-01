0c0071f499d2663aa18b8cf1c86c4a3d5a1c3c85
Oren Amsalem <oren.a4@gmail.com>
2019-09-01 17:32:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Efficient analytical reduction of nonlinear detailed neuron models",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/orena1/neuron_reduce",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2",
